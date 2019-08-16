124abe2c1b129be1fa570762851fb7b3344b0883
sacken0923 <sacken0923@gmail.com>
2019-08-16 15:57:45 +0900

    自動更新

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,8 @@
 class Api::MessagesController < ApplicationController
+  
   def index
-    @group = Group.find(params[:group_id]) 
-    @messages = @group.messages.includes(:user).where('id > ?', params[:last_id])
+    @messages = Message.includes(:user).where('id > ? and group_id = ?', params[:last_id], params[:group_id])
+
     respond_to do |format|
       format.html
       format.json 
